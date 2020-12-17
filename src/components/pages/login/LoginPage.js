d7149004d9e89db9f8b5cffef3616a5da6a1461a
MarcelGeirhos <geirhos.marcel94@gmail.com>
2020-12-17 21:49:12 +0100

    Login Form
    - Login Form und erstes Styling wurde hinzugefügt.

--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+// css imports
+import './LoginPage.css';
+import '../../gui/inputs/Inputfield.css';
+import '../../gui/buttons/Mainbutton.css';
+
 function LoginPage() {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login-form">
+            <form>
+                <h1>Login</h1>
+                <input type="email" placeholder="E-Mail..." className="inputfield" />
+                <input type="password" placeholder="Passwort..." className="inputfield" />
+                <p>Passwort vergessen?</p>
+                <button className="main-button">Einloggen</button>
+            </form>
         </div>
     );
 }
