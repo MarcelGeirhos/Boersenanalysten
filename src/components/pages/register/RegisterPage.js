40faef4b3be426d1ff2fab2fea3ffe8ae9d661d7
MarcelGeirhos <geirhos.marcel94@gmail.com>
2020-12-27 15:41:00 +0100

    Benutzer zur Datenbank hinzugefügt
    - Neue Benutzer werden nun zur Datenbank hinzugefügt mit den folgenden Informationen: Benutzername, E-Mail, uid und dem Erstellungsdatum.

--- src/components/pages/register/RegisterPage.js
+++ src/components/pages/register/RegisterPage.js
@@ -14,16 +14,17 @@ import { useDispatch } from 'react-redux';
 function RegisterPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
     
     const [routeRedirect, setRedirect] = useState(false);
     const dispatch = useDispatch();
-    const registerUserAction = (email, password) => dispatch(registerUser(email, password));
+    const registerUserAction = (email, password, username) => dispatch(registerUser(email, password, username));
 
     const register = async (e) => {
         e.preventDefault();
         // TODO
-        if (email !== "" && password !== "") {
-            await registerUserAction(email, password);
+        if (email !== "" && password !== "" && username !== "") {
+            await registerUserAction(email, password, username);
             setRedirect(true);
         } else {
             console.log("Leere Eingabefelder");
@@ -42,7 +43,7 @@ function RegisterPage() {
                 <h1>Registrieren</h1>
                 <Inputfield type="email" placeholder="E-Mail..." onChange={(e) => setEmail(e.target.value)} />
                 <Inputfield type="password" placeholder="Passwort..." onChange={(e) => setPassword(e.target.value)} />
-                <Inputfield type="text" placeholder="Benutzername..." />
+                <Inputfield type="text" placeholder="Benutzername..." onChange={(e) => setUsername(e.target.value)} />
                 <input type="submit" value="Registrieren" id="register-button" className="main-button" />
             </form>
         </div>
--- src/redux/actions/RegisterAction.js
+++ src/redux/actions/RegisterAction.js
@@ -1,8 +1,8 @@
 import firebase from '../../firebase/Config';
 
-export const registerUser = (email, password) => {
+export const registerUser = (email, password, username) => {
     return async function(dispatch) {
-        const user = await firebase.register(email, password);
+        const user = await firebase.register(email, password, username);
         console.log(user);
         dispatch({type: 'REGISTER_USER', payload: user});
     }
