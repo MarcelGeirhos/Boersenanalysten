070b01d74557f6fc1778f706b885bbef622b3276
MarcelGeirhos <geirhos.marcel94@gmail.com>
2021-01-02 23:47:45 +0100

    Dead Code entfernt
    - Dead Code von getArticleData entfernt.

--- src/components/pages/articleList/ArticleListPage.js
+++ src/components/pages/articleList/ArticleListPage.js
@@ -4,17 +4,14 @@ import React, { useState, useEffect } from 'react';
 import Listitem from './listitem/Listitem';
 import Mainbutton from '../../gui/buttons/Mainbutton';
 import { getArticleList } from '../../../redux/actions/ArticleActions';
-//import firebase from '../../../firebase/Config';
 
 // css imports
 import './ArticleListPage.css';
-import firebase from 'firebase/app';
 
 // third party imports
-import { useDispatch } from 'react-redux';
+import firebase from 'firebase/app';
 
 function ArticleListPage() {
-    // TODO durch richtige Daten aus der Datenbank ersetzen.
     const [articleList, setArticleList] = useState([]);
 
     useEffect(() => {
@@ -23,42 +20,8 @@ function ArticleListPage() {
             console.log(data);
             setArticleList(data.docs.map(doc => ({...doc.data()})))
         }
-        fetchData()
+        fetchData();
     }, [])
-
-    {/*const getArticles = async () => {
-        const list = [];
-        let snapshot = firebase.getArticleList().then(articleList => {
-            console.log(articleList);
-        })
-        //snapshot.forEach(element => {
-        //    list.push(element.data())
-        //});
-        setArticleList([articleList]);
-    }
-
-    useEffect(() => {
-        {firebase.getArticleList().then(articleList => {
-            console.log(articleList);
-            article = articleList;
-            console.log(article);
-        })}
-        getArticles();
-    }, []);*/}
-
-    function getArticleListFunction(item) {
-        let title = item.title;
-        console.log("Title: " + title);
-        return title;
-    }
-
-    function test() {
-        console.log("Test " + articleList.length);
-        for (let i = 0; i < articleList.length; i++) {
-            console.log(articleList);
-            return <p>{articleList[i].title}</p>
-        }
-    }
 
     return (
         <div>
@@ -67,22 +30,11 @@ function ArticleListPage() {
                 <Mainbutton link="/createArticle">Beitrag erstellen</Mainbutton>
             </div>
             <ul className="articlelist">
-                {/*{list.map(item => {
-                    return <Listitem key={item}>{item}</Listitem>
-                })}*/}
-                
-                {/*{articleList.map(item => {
-                    console.log("Title: " + item);
-                    return <Listitem title={item.title} key={item.title}>{item.title}</Listitem>
-                })}*/}
-                {/*{test()}*/}
                 {
-                    articleList.map(article => (
-                        <li>{article.title}</li>
-                    ))
+                articleList.map(article => (
+                    <li>{article.title}</li>
+                ))
                 }
-                
-            
             </ul>
         </div>
     );
--- src/redux/reducers/ArticleReducer.js
+++ src/redux/reducers/ArticleReducer.js
@@ -5,8 +5,6 @@ const article = (
     action) => {
         if (action.type === 'CREATE_ARTICLE') {
             state = {...state, article: action.payload}
-        } else if (action.type === 'GET_ARTICLE_LIST') {
-            state = {...state, article: action.payload}
         }
         return state;
     }
