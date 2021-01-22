a56b27fe2119ac9ca0ddbf25b00f4de34fb9fa35
MarcelGeirhos <geirhos.marcel94@gmail.com>
2021-01-22 19:03:15 +0100

    Beitrag erstellen optimiert
    - Es kann nun ein Beitrag erstellt werden der mit folgenden Daten in der Datenbank abgespeichert wird:
    - Titel
    - Beitragstext
    - Tags
    - Voting (0)
    - Antwortanzahl (0)
    - Ansichten (0)
    - Erstellungsdatum
    - eindeutige id

--- src/components/pages/createArticle/CreateArticlePage.js
+++ src/components/pages/createArticle/CreateArticlePage.js
@@ -11,11 +11,21 @@ import './CreateArticlePage.css';
 
 // third party imports
 import { Redirect } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import firebase from 'firebase/app';
 
+// material-ui icon imports
+import { Cancel } from '@material-ui/icons';
+
 function CreateArticlePage() {
-    const selectedTags = async tags => {
+    const [title, setTitle] = useState("");
+    const [tags, setTags] = useState([]);
+
+    const [routeRedirect, setRedirect] = useState(false);
+
+    // Der Artikel Text wird mit allen Eigenschaften die vom
+    // Benutzer mit dem Editor gesetzt wurden in die Datenbank
+    // geschrieben z.B. fetter Text oder unnummerierte Listen.
+    async function createNewArticle() {
         if (title !== "" && document.getElementById('text').innerHTML !== "" && tags !== "") {
             console.log(tags);
             const newArticle = await firebase.firestore().collection('articles').doc();
@@ -36,59 +46,49 @@ function CreateArticlePage() {
             console.log("Leere Eingabefelder");
         }
     }
-    const [title, setTitle] = useState("");
-
-    const [routeRedirect, setRedirect] = useState(false);
-    const dispatch = useDispatch();
-    const createArticleAction = (title, articleText, selectedTags) => dispatch(createArticle(title, articleText, selectedTags));
-
-    // Der Artikel Text wird mit allen Eigenschaften die vom
-    // Benutzer mit dem Editor gesetzt wurden in die Datenbank
-    // geschrieben z.B. fetter Text oder unnummerierte Listen.
-    const createNewArticle = async (e) => {
-        e.preventDefault();
-        /*if (title !== "" && document.getElementById('text').innerHTML !== "" && selectedTags !== "") {
-            console.log(selectedTags);
-            const newArticle = await firebase.firestore().collection('articles').doc();
-            const newArticleRef = await newArticle.get();
-            await firebase.firestore().collection('articles').doc(newArticleRef.id).set({
-            title: title,
-            articleText: document.getElementById('text').innerHTML,
-            tags: selectedTags,
-            views: 0,
-            voting: 0,
-            answerCounter: 0,
-            createdAt: new Date(),
-            id: newArticleRef.id,
-        })
-            //await createArticleAction(title, document.getElementById('text').innerHTML, selectedTags.document);
-            setRedirect(true);
-            console.log('Neuer Artikel wurde erstellt.');
-        } else {
-            console.log("Leere Eingabefelder");
-        }*/
-    }
 
     const redirectTo = routeRedirect;
     if (redirectTo) {
         return <Redirect to="/articleList" />
     }
 
+    const addTags = event => {
+        console.log('Add Tag');
+        if (event.key === "Enter" && event.target.value !== "") {
+            setTags([...tags, event.target.value]);
+            event.target.value = "";
+        }
+    }
+
+    const removeTags = indexToRemove => {
+        setTags(tags.filter((_, index) => index !== indexToRemove));
+    }
+
     return (
         <div className="create-article">
-            <form onSubmit={createNewArticle}>
-                <h1>Beitrag erstellen</h1>
-                <div className="create-article-title">
-                    <label>Titel:</label>
-                    <InputfieldDark type="text" placeholder="Beitragstitel..." onChange={(e) => setTitle(e.target.value)} ></InputfieldDark>
-                </div>
-                <TextEditor title="Beitrag:" />
-                <label>Tags:</label>
-                <TagInput selectedTags={selectedTags}/>
-                <div>
-                    <input type="submit" value="Beitrag erstellen" id="create-article-button" className="main-button" /*onClick={() => createNewArticle(selectedTags)}*//>
-                </div>
-            </form>
+            <h1>Beitrag erstellen</h1>
+            <div className="create-article-title">
+                <label>Titel:</label>
+                <InputfieldDark type="text" placeholder="Beitragstitel..." onChange={(e) => setTitle(e.target.value)} ></InputfieldDark>
+            </div>
+            <TextEditor title="Beitrag:" />
+            <label>Tags:</label>
+            <div className="tags-input">
+                <input type="text" placeholder="Enter drücken zum hinzufügen..." onKeyUp={e => (e.key === "Enter" ? addTags(e) : null)} />
+                <ul id="tags">
+                {
+                    tags.map((tag, index) => (
+                        <li key={index} className="tag-listitem">
+                        <span className="tag-title">{tag}</span>
+                        <span className="tag-close-icon" onClick={() => removeTags(index)}><Cancel /></span>
+                    </li>)
+                    )
+                }
+                </ul>
+            </div>
+            <div>
+                <input value="Beitrag erstellen" id="create-article-button" className="main-button" onClick={() => createNewArticle()}/>
+            </div>
         </div>
     );
 }
