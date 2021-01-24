97de7d6baa1b0c8e124fcd496aec7588bc19b586
MarcelGeirhos <geirhos.marcel94@gmail.com>
2021-01-24 14:45:18 +0100

    Voting hinzugefügt
    - Das Voting von Beiträgen wurde hinzugefügt muss noch verbessert werden (Realtime Update und nur 1 Vote pro Benutzer pro Beitrag).

--- src/components/pages/article/voting/Voting.js
+++ src/components/pages/article/voting/Voting.js
@@ -9,12 +9,26 @@ import {
     ArrowDropDown,
 } from '@material-ui/icons';
 
+// third party imports
+import firebase from 'firebase/app';
+import { useParams } from "react-router-dom";
+
 const Voting = (props) => {
+    const { id } = useParams();
+
+    const setVoting = async (vote) => {
+        const article = await firebase.firestore().collection('articles').doc(id).get();
+        await firebase.firestore().collection('articles').doc(id).update({
+            voting: article.data().voting + vote,
+        });
+        console.log('Voting wurde erfolgreich gewertet.');
+    }
+
     return (
         <div className="voting-section">
-            <button className="voting-button" /*onClick={() => editorButtonsHandler('bold')}*/><ArrowDropUp /></button>
+            <button className="voting-button" onClick={() => setVoting(1)}><ArrowDropUp /></button>
             <p>{props.voting}</p>
-            <button className="voting-button" /*onClick={() => editorButtonsHandler('bold')}*/><ArrowDropDown /></button>
+            <button className="voting-button" onClick={() => setVoting(-1)}><ArrowDropDown /></button>
         </div>
     );
 }
