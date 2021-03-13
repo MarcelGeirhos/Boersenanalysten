a0db55ea38b939074bf5428a5b13be5550e51d0f
MarcelGeirhos <geirhos.marcel94@gmail.com>
2021-03-13 12:14:38 +0100

    Beiträge löschen
    - Beiträge können gelöscht werden.
    - Kleinere Code Verbesserungen wurden implementiert.

--- src/components/pages/article/ArticlePage.js
+++ src/components/pages/article/ArticlePage.js
@@ -13,34 +13,55 @@ import './ArticlePage.css';
 
 // third party imports
 import firebase from 'firebase/app';
-import { useParams, Link } from "react-router-dom";
+import {
+    useParams,
+    Redirect,
+    Link,
+} from "react-router-dom";
+
+// material-ui imports
+import {
+    Slide,
+    Button,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+} from '@material-ui/core';
+
+const DialogTransition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 function ArticlePage() {
     const { id } = useParams();
     const [tagList, setTagList] = useState([]);
     const [userData, setUserData] = useState([]);
     const [answerList, setAnswerList] = useState([]);
-    const [editorText, setEditorText] = useState("");
+    const [answerText, setAnswerText] = useState("");
     const [articleData, setArticleData] = useState("");
     const [articleText, setArticleText] = useState("");
+    const [openDialog, setOpenDialog] = useState(false);
+    const [routeRedirect, setRedirect] = useState(false);
     const [answerErrorText, setErrorAnswerText] = useState("");
     const [answerCreatedAt, setAnswerCreatedAt] = useState([]);
     const [articleCreatedAt, setArticleCreatedAt] = useState("");
-    const dateOptions = { year: '2-digit', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit'};
+    const dateOptions = { year: '2-digit', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' };
 
     useEffect(() => {
         const fetchData = async () => {
             // Artikel Daten werden ausgelesen und gesetzt
             const articleData = await firebase.firestore().collection('articles').doc(id).get();
             setArticleCreatedAt(articleData.data().createdAt.toDate().toLocaleDateString("de-DE", dateOptions));
-            setArticleData({...articleData.data()});
+            setArticleData({ ...articleData.data() });
             setTagList(articleData.data().tags);
-            
+
             // Alle Antworten Daten werden ausgelesen und gesetzt
             const answerData = await firebase.firestore().collection('articles').doc(id).collection('answers').get();
             setAnswerCreatedAt(answerData.docs.map(doc => (doc.data().createdAt.toDate().toLocaleDateString("de-DE", dateOptions))));
-            setAnswerList(answerData.docs.map(doc => ({...doc.data()})));
-           
+            setAnswerList(answerData.docs.map(doc => ({ ...doc.data() })));
+
             // TODO Text mit richtiger Formatierung anzeigen lassen.
             const articleTextWithHTML = articleData.data().articleText;
             console.log(articleTextWithHTML);
@@ -75,47 +96,68 @@ function ArticlePage() {
     // Der Antwort Text wird mit allen Eigenschaften die vom
     // Benutzer mit dem Editor gesetzt wurden in die Datenbank
     // geschrieben z.B. fetter Text oder unnummerierte Listen, etc. .
-    const createNewAnswer = async (e) => {
-        let isAnswerTextValid = checkAnswerText();
-        if (isAnswerTextValid) {
-            const article = await firebase.firestore().collection('articles').doc(id);
-            const articleRef = await article.get();
-            const newAnswerRef = await firebase.firestore().collection('articles').doc(articleRef.id).collection('answers').doc();
-            await firebase.firestore().collection('articles').doc(articleRef.id).collection('answers').doc(newAnswerRef.id).set({
-                answerText: document.getElementById('text').innerHTML,
+    const createNewAnswer = async () => {
+        if (isAnswerTextValid() == true) {
+            const newAnswer = await firebase.firestore().collection('articles').doc(id).collection('answers').doc();
+            await firebase.firestore().collection('articles').doc(id).collection('answers').doc(newAnswer.id).set({
+                answerText: answerText,
                 voting: 0,
                 createdAt: new Date(),
-                id: newAnswerRef.id,
+                id: newAnswer.id,
                 creator: userData.username,
                 creatorId: userData.uid,
             });
             await firebase.firestore().collection('articles').doc(id).update({
                 answerCounter: articleData.answerCounter + 1,
             });
-            await firebase.firestore().collection('users').doc(userData.uid).collection('answers').doc(newAnswerRef.id).set({
-                articleRef: firebase.firestore().doc(`/articles/${articleRef.id}/answers/${newAnswerRef.id}`),
+            await firebase.firestore().collection('users').doc(userData.uid).collection('answers').doc(newAnswer.id).set({
+                articleRef: firebase.firestore().doc(`/articles/${id}/answers/${newAnswer.id}`),
             })
             window.location.reload();
             console.log('Neue Antwort wurde erfolgreich erstellt.');
-        } else {
-            console.log("Leere Eingabefelder");
         }
     }
 
-    // Verbindung zu TagInput Komponente um auf die eingegebenen Tags 
+    // Verbindung zu Editor Komponente um auf den eingegebenen Antworttext 
     // Zugriff zu bekommen.
-    const callbackEditorText = (editorText) => {
-        setEditorText(editorText);
+    const callbackEditorText = (answerText) => {
+        setAnswerText(answerText);
     }
 
-    const checkAnswerText = () => {
-        if (editorText === "") {
+    const isAnswerTextValid = () => {
+        if (answerText === "") {
             setErrorAnswerText('Bitte geben Sie ihre Antwort ein.');
             document.getElementById("answer-error-text").style.visibility = "visible";
+            console.log("Es wurde keine Antwort eingegeben.");
             return false;
         }
         document.getElementById("answer-error-text").style.visibility = "hidden";
         return true;
+    }
+
+    const handleClickOpen = () => {
+        setOpenDialog(true);
+    };
+
+    const handleClose = () => {
+        setOpenDialog(false);
+    };
+
+    const deleteArticle = async () => {
+        await firebase.firestore().collection("articles").doc(id).collection("answers").get().then((querySnapshot) => {
+            querySnapshot.forEach((answer) => {
+                firebase.firestore().collection("articles").doc(id).collection("answers").doc(answer.id).delete();
+            });
+        });
+        await firebase.firestore().collection('articles').doc(id).delete();
+        setOpenDialog(false);
+        alert('Beitrag wurde erfolgreich gelöscht.');
+        setRedirect(true);
+    }
+
+    const redirectTo = routeRedirect;
+    if (redirectTo) {
+        return <Redirect to="/articleList" />
     }
 
     return (
@@ -126,30 +168,56 @@ function ArticlePage() {
                     <h1>{articleData.title}</h1>
                     <p>{articleText}</p>
                     {
-                    tagList.map(tag => (
-                        <Tagbutton>{tag}</Tagbutton>
-                    ))
-                    } 
+                        tagList.map(tag => (
+                            <Tagbutton>{tag}</Tagbutton>
+                        ))
+                    }
+                    <div>
+                        <Button>Bearbeiten</Button>
+                        <Button onClick={handleClickOpen}>Löschen</Button>
+                        <Dialog
+                            PaperProps={{
+                                style: {
+                                    backgroundColor: '#212121',
+                                },
+                            }}
+                            open={openDialog}
+                            onClose={handleClose}
+                            TransitionComponent={DialogTransition}
+                            keepMounted
+                            disabled>
+                            <DialogTitle className="dialog-title">Beitrag löschen?</DialogTitle>
+                            <DialogContent className="dialog-content">
+                                <DialogContentText>
+                                    Wollen Sie den Beitrag wirklich unwideruflich löschen?
+                                </DialogContentText>
+                            </DialogContent>
+                            <DialogActions className="dialog-actions">
+                                <Button onClick={handleClose}>Nein</Button>
+                                <Button onClick={deleteArticle}>Ja</Button>
+                            </DialogActions>
+                        </Dialog>
+                    </div>
                 </div>
                 <div className="user-info-section">
                     <p>{articleCreatedAt}</p>
-                    <Link to={{pathname: `/userprofile/${articleData.creatorId}`}}>
+                    <Link to={{ pathname: `/userprofile/${articleData.creatorId}` }}>
                         {articleData.creator}
                     </Link>
                 </div>
             </div>
             <h2>Antworten:</h2>
             {
-            answerList.map((answer, index) => (
-                <Answeritem id={answer.id}
-                    answerText={answer.answerText}
-                    voting={answer.voting}
-                    creator={answer.creator}
-                    creatorId={answer.creatorId}
-                    createdAt={answerCreatedAt[index]}></Answeritem>
-            ))
+                answerList.map((answer, index) => (
+                    <Answeritem id={answer.id}
+                        answerText={answer.answerText}
+                        voting={answer.voting}
+                        creator={answer.creator}
+                        creatorId={answer.creatorId}
+                        createdAt={answerCreatedAt[index]}></Answeritem>
+                ))
             }
-            <TextEditor title="Antwort:" parentCallbackText={callbackEditorText}/>
+            <TextEditor title="Antwort:" parentCallbackText={callbackEditorText} />
             <ErrorText id="answer-error-text">{answerErrorText}</ErrorText>
             <div>
                 <input
